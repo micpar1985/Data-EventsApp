7f2cdb49b2bb79306bb7defcf3e1cd4e294d2b0e
user <user@this.com>
2020-08-04 18:58:18 -0400

    Spring initializer template

---
+++ data_services/src/test/java/com/event_app/data_services/DataServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event_app.data_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
