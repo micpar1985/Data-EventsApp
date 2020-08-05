454291811e93313582af6de6b64ca97604db46c4
Husam Elsheikh <Elsheikh_HusamEldeen@bah.com>
2020-08-05 18:19:37 -0400

    Finished model classes for Event and Registration.

--- data_services/src/main/java/com/event_app/data_services/model/Event.java
+++ data_services/src/main/java/com/event_app/data_services/model/Event.java
@@ -5,4 +5,65 @@ import java.io.Serializable;
 public class Event implements Serializable {
 
 	private final static long serialVersionUID = 1L;
+	
+	private Long id;
+	private String code;
+	private String title;
+	private String description;
+	
+	public Event(Long id, String code, String title, String description) {
+		this.id = id;
+		this.code = code;
+		this.title = title;
+		this.description = description;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the code
+	 */
+	public String getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(String code) {
+		this.code = code;
+	}
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+	/**
+	 * @param title the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- data_services/src/main/java/com/event_app/data_services/model/Registration.java
+++ data_services/src/main/java/com/event_app/data_services/model/Registration.java
@@ -1,5 +1,86 @@
 package com.event_app.data_services.model;
 
-public class Registration {
+import java.io.Serializable;
+import java.util.Date;
 
+public class Registration implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private Long event_id;
+	private Long customer_id;
+	private Date registration_date;
+	private String notes;
+	
+	public Registration(Long id, Long event_id, Long customer_id, Date registration_date, String notes) {
+		this.id = id;
+		this.event_id = event_id;
+		this.customer_id = customer_id;
+		this.registration_date = registration_date;
+		this.notes = notes;
+	}
+	
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return the event_id
+	 */
+	public Long getEvent_id() {
+		return event_id;
+	}
+	/**
+	 * @param event_id the event_id to set
+	 */
+	public void setEvent_id(Long event_id) {
+		this.event_id = event_id;
+	}
+	/**
+	 * @return the customer_id
+	 */
+	public Long getCustomer_id() {
+		return customer_id;
+	}
+	/**
+	 * @param customer_id the customer_id to set
+	 */
+	public void setCustomer_id(Long customer_id) {
+		this.customer_id = customer_id;
+	}
+	/**
+	 * @return the registration_date
+	 */
+	public Date getRegistration_date() {
+		return registration_date;
+	}
+	/**
+	 * @param registration_date the registration_date to set
+	 */
+	public void setRegistration_date(Date registration_date) {
+		this.registration_date = registration_date;
+	}
+	/**
+	 * @return the notes
+	 */
+	public String getNotes() {
+		return notes;
+	}
+	/**
+	 * @param notes the notes to set
+	 */
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
+	
 }
