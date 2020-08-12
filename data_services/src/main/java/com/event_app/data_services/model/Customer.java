4abd882f248ae7d24cf566f56436dfd775397a11
Husam Elsheikh <Elsheikh_HusamEldeen@bah.com>
2020-08-11 20:41:30 -0400

    Added JPA dependency to build.gradle and added data.sql

--- data_services/src/main/java/com/event_app/data_services/model/Customer.java
+++ data_services/src/main/java/com/event_app/data_services/model/Customer.java
@@ -1,7 +1,9 @@
 package com.event_app.data_services.model;
 
 import java.io.Serializable;
+import javax.persistence.*;
 
+@Entity
 public class Customer implements Serializable{
 
 	private static final long serialVersionUID = 1L;
---
+++ data_services/src/main/resources/data.sql
@@ -0,0 +1,22 @@
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Bob', 'B0bI$Cool', 'bob@gmail.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Sarah', 'Pa$$w0rd', 'sarah@gmail.com');
+insert into CUSTOMER (NAME, PASSWORD, EMAIL) VALUES ('Joe', '$AfCat9&=uANt','joe@yahoo.com');
+
+insert into EVENT (CODE, TITLE, DESCRIPTION) VALUES ('BEE', 'Best Event Ever', 'This event is great!');
+insert into EVENT (CODE, TITLE, DESCRIPTION) VALUES ('JBP', 'Joes Birthday Party', 'Everyone is invited!');
+insert into EVENT (CODE, TITLE, DESCRIPTION) VALUES ('SSN', 'Study Session', 'Math is Fun');
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values('2', '3', '2020-10-10 15:00:00.0', 'Hosting');
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values('1', '1', '2020-10-10 00:00:00.0', 'Excited');
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values('2', '2', '2020-10-10 15:00:00.0', 'Bringing snacks');
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values('1', '2', '2020-10-10 00:00:00.0', 'this is a note');
+
+insert into REGISTRATION (EVENT_ID, CUSTOMER_ID, REGISTRATION_DATE, NOTES)
+values('3', '1', '2020-10-10 15:00:00.0', 'Math is not fun');
