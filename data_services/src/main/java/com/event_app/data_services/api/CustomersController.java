3ed941ecbdc61664cf017f9094e36d9475fb994b
michaelparish <micpar1985@gmail.com>
2020-08-12 17:55:32 -0400

    Adding endpoints to customer controller

--- data_services/src/main/java/com/event_app/data_services/api/CustomersController.java
+++ data_services/src/main/java/com/event_app/data_services/api/CustomersController.java
@@ -3,9 +3,12 @@ package com.event_app.data_services.api;
 import com.event_app.data_services.model.Customer;
 import com.event_app.data_services.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 import java.util.Collection;
 
 @RestController
@@ -29,9 +32,40 @@ public class CustomersController {
 
     @RequestMapping("/byName/{name}")
     public Customer findCustomerByName(@PathVariable("name") String name) {
+
         return customerService.findByName(name);
     }
 
+    @PostMapping
+    public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer,
+                                         UriComponentsBuilder uri) {
 
+        if (newCustomer.getId() != 0 || newCustomer.getName() == null
+                || newCustomer.getEmail() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        newCustomer=customerService.save(newCustomer);
+        URI location= ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+        ResponseEntity<?> response=ResponseEntity.created(location).build();
+        return response;
+    }
 
+    @PutMapping("/{customerId}")
+    public ResponseEntity<?> putCustomer(@RequestBody Customer newCustomer,
+                                         @PathVariable("customerId") long customerId) {
+        if (newCustomer.getId() != customerId
+                || newCustomer.getName() == null
+                || newCustomer.getEmail() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        newCustomer = customerService.save(newCustomer);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteById(id);
+    }
 }
+
