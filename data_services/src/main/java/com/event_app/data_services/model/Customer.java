74f184a66cfe438e8dab91ca8f162b16f915ea45
Husam Elsheikh <Elsheikh_HusamEldeen@bah.com>
2020-08-11 21:07:38 -0400

    Added JPA annotations to model classes and updated application.properties.

--- data_services/src/main/java/com/event_app/data_services/model/Customer.java
+++ data_services/src/main/java/com/event_app/data_services/model/Customer.java
@@ -1,13 +1,20 @@
 package com.event_app.data_services.model;
 
 import java.io.Serializable;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Customer")
 public class Customer implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String password;
