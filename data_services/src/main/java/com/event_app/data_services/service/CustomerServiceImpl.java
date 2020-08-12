113dc5ac9ff1fcc68247ac775147bce3009c571d
Mekdes Seyoum <mekdes.seyoum@yahoo.com>
2020-08-12 17:19:42 -0400

    customer service

--- data_services/src/main/java/com/event_app/data_services/service/CustomerServiceImpl.java
+++ data_services/src/main/java/com/event_app/data_services/service/CustomerServiceImpl.java
@@ -1,7 +1,9 @@
 package com.event_app.data_services.service;
 
 import java.util.Collection;
+
 import java.util.List;
+import java.util.Optional;
 
 import com.event_app.data_services.model.Customer;
 import com.event_app.data_services.repository.CustomerRepository;
@@ -16,23 +18,55 @@ public class CustomerServiceImpl implements CustomerService{
 	@Autowired
 	private CustomerRepository customerRepository;
 	
-	@Override
-	public Customer findById(Long id) {
-
-		return customerRepository.findById(id);
-		
-	}
+	/*
+	 * @Override public Customer findById(Long id) {
+	 * 
+	 * return customerRepository.findById(id);
+	 * 
+	 * }
+	 */
 	
-	@Override 
-	public Collection<Customer> findAll(){
+	/*
+	 * @Override public Collection<Customer> findAll(){
+	 * 
+	 * return customerRepository.findAll(); }
+	 * 
+	 * @Override public Customer findByName(String name){
+	 * 
+	 * return customerRepository.findByName(name); }
+	 */
+	
+	
+	@Override
+	public Iterable<Customer> findAllCustomers(){
 		
 		return customerRepository.findAll();
 	}
 	
 	@Override
-	public Customer findByName(String name){
-		
-		return customerRepository.findByName(name);
+	public Optional<Customer> findCustomerById(long id){
+	
+		return customerRepository.findById(id);
 	}
-
+	
+    @Override
+	public void save(Customer customer){
+		
+		return customerRepository.save(customer);
+	}
+	
+	@Override
+	public void update(Customer customer) {
+		
+		return customerRepository.update(customer);
+		
+	}
+	
+	@Override
+	public void delete(Long id) {
+		
+		return customerRepository.remove(id);
+		
+		
+	}
 }
